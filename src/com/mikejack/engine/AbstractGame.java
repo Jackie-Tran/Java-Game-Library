93681d7a761074c298088aabde5689683a94d29a
L-Michael1 <micchhael21@gmail.com>
2019-07-12 18:58:09 -0400

    Added GameState class

--- src/com/mikejack/engine/AbstractGame.java
+++ src/com/mikejack/engine/AbstractGame.java
@@ -1,9 +1,15 @@
 package com.mikejack.engine;
 
+import com.mikejack.gamestate.GameStateManager;
+
 public abstract class AbstractGame {
-
-	public abstract void update(GameContainer gc);
-
+	
+	protected GameStateManager gsm = new GameStateManager();
+	
+	public void update(GameContainer gc) {
+		gsm.update();
+	}
+	
 	public abstract void render(GameContainer gc);
 
 }
