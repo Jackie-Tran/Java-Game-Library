327b3f1dfcd55443f7a502a73a990d0d88cff52b
Jackie-Tran <jtran2009a@gmail.com>
2019-07-13 14:41:15 -0400

    Fixed michael's silly bug

--- src/com/mikejack/engine/GameContainer.java
+++ src/com/mikejack/engine/GameContainer.java
@@ -13,133 +13,136 @@ import java.util.Random;
 //Game engine = engine starts up game loop and loads all resources and controls user inputs
 public class GameContainer extends Canvas implements Runnable {
 
-	private Window window;
-	private AbstractGame game;
+    public static int UPDATES, FPS;
+    
+    private Window window;
+    private AbstractGame game;
 
-	private Thread gameThread;
-	private boolean running = false;
-	// window variables
-	private int width = 320, height = 240;
-	private int scale = 3;
-	private String title = "MiJakEngine v0.1";
+    private Thread gameThread;
+    private boolean running = false;
+    // window variables
+    private int width = 320, height = 240;
+    private int scale = 3;
+    private String title = "MiJakEngine v0.1";
 
-	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-	private Screen screen;
-	private Input input;
+    private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    private Screen screen;
+    private Input input;
 
-	public GameContainer(AbstractGame game) {
-		this.game = game;
-		window = new Window(title, width, height, scale, this);
-		screen = new Screen(this);
-		input = new Input(this);
+    public GameContainer(AbstractGame game) {
+	this.game = game;
+	window = new Window(title, width, height, scale, this);
+	screen = new Screen(this);
+	input = new Input(this);
 
+    }
+
+    public void init() {
+    }
+
+    public synchronized void start() {
+	if (running)
+	    return;
+	running = true;
+	gameThread = new Thread(this);
+	gameThread.start();
+
+    }
+
+    public synchronized void stop() {
+	running = false;
+	if (gameThread != null) {
+	    try {
+		gameThread.join();
+	    } catch (InterruptedException e) {
+		e.printStackTrace();
+	    }
+	}
+    }
+
+    public void run() {
+	long lastTime = System.nanoTime();
+	double amountOfTicks = 60.0;
+	double ns = 1000000000 / amountOfTicks;
+	double delta = 0;
+	long timer = System.currentTimeMillis();
+	int updates = 0;
+	int frames = 0;
+
+	init();
+	while (running) {
+	    long now = System.nanoTime();
+	    delta += (now - lastTime) / ns;
+	    lastTime = now;
+	    while (delta >= 1) {
+		update();
+		updates++;
+		delta--;
+	    }
+	    render();
+	    frames++;
+
+	    if (System.currentTimeMillis() - timer > 1000) {
+		timer += 1000;
+		UPDATES = updates;
+		FPS = frames;
+		System.out.println("UPDATES: " + updates + "\t FPS: " + frames);
+		frames = 0;
+		updates = 0;
+
+	    }
+	}
+    }
+
+    public void update() {
+	game.update(this);
+	input.update();
+
+    }
+
+    public Input getInput() {
+	return input;
+    }
+
+    public void render() {
+	BufferStrategy bs = getBufferStrategy();
+	if (bs == null) {
+	    createBufferStrategy(3);
+	    return;
 	}
 
-	public void init() {
-	}
+	Graphics g = bs.getDrawGraphics();
+	screen.clear();
+	game.render(this);
+	screen.drawText("FPS: " + Integer.toString(FPS), 0, 0, 0xffffffff);
+	g.drawImage(image, 0, 0, width * scale, height * scale, null);
+	g.dispose();
+	bs.show();
 
-	public synchronized void start() {
-		if (running)
-			return;
-		running = true;
-		gameThread = new Thread(this);
-		gameThread.start();
+    }
 
-	}
+    public int getImageWidth() {
+	return width;
+    }
 
-	public synchronized void stop() {
-		running = false;
-		if (gameThread != null) {
-			try {
-				gameThread.join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    public int getImageHeight() {
+	return height;
+    }
 
-	public void run() {
-		long lastTime = System.nanoTime();
-		double amountOfTicks = 60.0;
-		double ns = 1000000000 / amountOfTicks;
-		double delta = 0;
-		long timer = System.currentTimeMillis();
-		int updates = 0;
-		int frames = 0;
+    public Window getWindow() {
+	return window;
+    }
 
-		init();
-		while (running) {
-			long now = System.nanoTime();
-			delta += (now - lastTime) / ns;
-			lastTime = now;
-			while (delta >= 1) {
-				update();
-				updates++;
-				delta--;
-			}
-			render();
-			frames++;
+    public int getScale() {
+	return scale;
+    }
 
-			if (System.currentTimeMillis() - timer > 1000) {
-				timer += 1000;
+    public BufferedImage getImage() {
+	return image;
+    }
 
-				screen.drawText("UPDATES :" + updates + " FPS: " + frames, 0, 0, 0xff00ffff);
-				System.out.println("UPDATES: " + updates + "\t FPS: " + frames);
-				frames = 0;
-				updates = 0;
-				
-			}
-		}
-	}
-
-	public void update() {
-		game.update(this);
-		input.update();
-
-	}
-
-	public Input getInput() {
-		return input;
-	}
-
-	public void render() {
-		BufferStrategy bs = getBufferStrategy();
-		if (bs == null) {
-			createBufferStrategy(3);
-			return;
-		}
-
-		Graphics g = bs.getDrawGraphics();
-		screen.clear();
-		game.render(this);
-		g.drawImage(image, 0, 0, width * scale, height * scale, null);
-		g.dispose();
-		bs.show();
-
-	}
-
-	public int getImageWidth() {
-		return width;
-	}
-
-	public int getImageHeight() {
-		return height;
-	}
-
-	public Window getWindow() {
-		return window;
-	}
-
-	public int getScale() {
-		return scale;
-	}
-
-	public BufferedImage getImage() {
-		return image;
-	}
-
-	public Screen getScreen() {
-		return screen;
-	}
+    public Screen getScreen() {
+	return screen;
+    }
 
 }
